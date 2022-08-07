43a9a242882b098d7be08b62a34369b3d7a38ae2
Mostafa Khattat <m.khattat@student.tudelft.nl>
2022-08-07 16:07:48 +0200

    hardcode sshfs version

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 echo Start
 export HOME=/data/data/com.termux/files/home
 export PATH=/data/data/com.termux/files/usr/bin/:$PATH
-# SSHFS_VERSION_LINK=$(curl -s https://github.com/libfuse/sshfs/releases/latest | cut -d'"' -f2 | cut -d'"' -f1)
-# export SSHFS_VERSION=${SSHFS_VERSION_LINK##*/}
 export SSHFS_VERSION="sshfs-3.7.3"
 export SSHFS_VERSION_LITE=${SSHFS_VERSION##*-}
 #set the link to the lastest .tar.xz tarball at https://github.com/libfuse/sshfs/releases
